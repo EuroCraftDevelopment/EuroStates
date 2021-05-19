b5e60dc4f167fa29757e1a913e00e68ee905b15f
ColonelKai <59742334+ColonelKai@users.noreply.github.com>
2021-05-20 01:35:31 +0300

    Initial commit

---
+++ src/main/java/org/eurostates/EuroStates.java
@@ -0,0 +1,17 @@
+package org.eurostates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EuroStates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //test
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
