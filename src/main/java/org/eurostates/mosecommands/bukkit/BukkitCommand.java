11c4c4f37a4b6d864757059ba6a5d94170f579a0
mosemister <nekronmoseley@gmail.com>
2021-05-30 21:18:46 +0100

    Added LegacyColourCodeArgument

--- src/main/java/org/eurostates/mosecommands/bukkit/BukkitCommand.java
+++ src/main/java/org/eurostates/mosecommands/bukkit/BukkitCommand.java
@@ -4,19 +4,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.eurostates.mosecommands.ArgumentCommand;
-import org.eurostates.mosecommands.ArgumentCommands;
 import org.eurostates.mosecommands.context.CommandContext;
 
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class BukkitCommand implements TabExecutor {
+
+    private Collection<ArgumentCommand> commands;
+
+    public BukkitCommand(ArgumentCommand... commands) {
+        this.commands = Arrays.asList(commands);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        CommandContext context = new CommandContext(sender, getCommands(), args);
+        CommandContext context = new CommandContext(sender, this.commands, args);
         Optional<ArgumentCommand> opCommand = context.getCompleteCommand();
         if (!opCommand.isPresent()) {
             context.getErrors().forEach(e -> sender.sendMessage(e.getError()));
@@ -31,17 +37,11 @@ public class BukkitCommand implements TabExecutor {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        CommandContext context = new CommandContext(sender, getCommands(), args);
+        CommandContext context = new CommandContext(sender, this.commands, args);
         return context
                 .getPotentialCommands()
                 .stream()
                 .flatMap(c -> context.getSuggestions(c).parallelStream())
                 .collect(Collectors.toList());
-    }
-
-    private Set<ArgumentCommand> getCommands() {
-        Set<ArgumentCommand> set = new HashSet<>();
-        set.add(ArgumentCommands.TOWN_VIEW);
-        return set;
     }
 }
