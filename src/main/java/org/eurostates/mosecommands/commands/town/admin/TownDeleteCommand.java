9b0f1bbaf1f58992158a32da2855d407a71e0f88
ColonelKai <59742334+ColonelKai@users.noreply.github.com>
2021-06-07 13:30:01 +0300

    [vA_3.4.7] NEW: TownDeleteCommand.java, TownCreateCommand.java

---
+++ src/main/java/org/eurostates/mosecommands/commands/town/admin/TownDeleteCommand.java
@@ -0,0 +1,61 @@
+package org.eurostates.mosecommands.commands.town.admin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.eurostates.area.state.CustomState;
+import org.eurostates.area.town.CustomTown;
+import org.eurostates.area.town.Town;
+import org.eurostates.mosecommands.ArgumentCommand;
+import org.eurostates.mosecommands.arguments.CommandArgument;
+import org.eurostates.mosecommands.arguments.area.TownArgument;
+import org.eurostates.mosecommands.arguments.operation.ExactArgument;
+import org.eurostates.mosecommands.context.CommandContext;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+public class TownDeleteCommand implements ArgumentCommand {
+    public static final ExactArgument ADMIN_ARGUMENT = new ExactArgument("admin");
+    public static final ExactArgument DELETE_ARGUMENT = new ExactArgument("delete");
+    public static final TownArgument TOWN_ARGUMENT = new TownArgument("town");
+
+    @Override
+    public @NotNull CommandArgument<?>[] getArguments() {
+        return new CommandArgument[]{
+                ADMIN_ARGUMENT,
+                DELETE_ARGUMENT,
+                TOWN_ARGUMENT
+        };
+    }
+
+    @Override
+    public Optional<String> getPermission() {
+        return Optional.of("eurostates.admin");
+    }
+
+    @Override
+    public boolean run(@NotNull CommandContext context, @NotNull String[] arg) {
+        Town town = context.getArgument(this, TOWN_ARGUMENT);
+        CustomState state = town.getState();
+
+        state.getTowns().remove(town);
+
+        File file = town.getOwnerUser().getFile();
+        boolean didDelete = file.delete();
+
+        if(!didDelete) Bukkit.getLogger().warning("Could not delete state file: " + file.toString());
+
+        try {
+            state.save();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bukkit.broadcastMessage(ChatColor.BLUE+"[EuroStates] "+
+                ChatColor.WHITE+"Town "+town.getName()+" was forcibly disbanded.");
+
+        return true;
+    }
+}
